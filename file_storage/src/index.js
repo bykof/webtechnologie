a66e65dabd79b1520c40a564b4d200fd6a3b714e
Michael Bykovski <mbykovski@seibert-media.net>
2017-05-01 22:43:06 +0200

    Upload files in the service

--- file_storage/src/index.js
+++ file_storage/src/index.js
@@ -4,13 +4,14 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import middleware from './middleware';
 import config from './config';
-import files from './api/files';
+import files, {file_custom_routes} from './api/files';
 import 'express-resource';
 import initDB from './db';
+import fileUpload from 'express-fileupload';
 
 let app = express();
 app.use(morgan('dev'));
-
+app.use(fileUpload());
 app.use(cors({
   exposedHeaders: config.corsHeaders
 }));
@@ -23,6 +24,7 @@ initDB(
   () => {
     app.use(middleware({config}));
     app.resource('files', files);
+    app.use(file_custom_routes);
     let listener = app.listen(process.env.PORT || config.port, function(){
       console.log('Listening on port ' + listener.address().port);
     });
