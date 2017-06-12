438c0a1f60c72a9b6225099b46c8f24ecace4bf1
Onur <onkaraman@outlook.com>
2017-06-12 13:57:21 +0200

    invoice_item: Chaned fields.

--- liabilities/src/models/invoice_item.js
+++ liabilities/src/models/invoice_item.js
@@ -2,9 +2,15 @@ import mongoose from "mongoose";
 
 let invoiceItemSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now },
+    settle_date: { type: Date },
     invoice_id: { type: String, required: true },
     user_id: { type: Number, required: true, unique: true, dropDups: true },
-    role: { type: String, enum: ["creditor", "debtor"] }
+    role: { type: String, enum: ["creditor", "debtor"] },
+    advanced_price: { type: Number, required: false},
+    settled: { type: Boolean }
 });
+
+invoiceItemSchema.statics.Settle = (money_amount) => {
+}
 
 export default mongoose.model("InvoiceItem", invoiceItemSchema);
